ffe9eeccf0dd83cdadfbd100690ecf279fd5d6d1
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2020-11-01 22:14:34 +0800

    add loading

--- js/index.js
+++ js/index.js
@@ -1,16 +1,17 @@
 
+const index=10;
 const makePoem = () => {
-    var str = document.getElementsByTagName('div')[4].innerHTML.toString();
+    var str = document.getElementsByTagName('div')[index].innerHTML.toString();
     var i = 0;
-    document.getElementsByTagName('div')[4].innerHTML = "";
+    document.getElementsByTagName('div')[index].innerHTML = "";
 
     setTimeout(function () {
         var se = setInterval(function () {
             i++;
-            document.getElementsByTagName('div')[4].innerHTML = str.slice(0, i) + "|";
+            document.getElementsByTagName('div')[index].innerHTML = str.slice(0, i) + "|";
             if (i == str.length) {
                 clearInterval(se);
-                document.getElementsByTagName('div')[4].innerHTML = str;
+                document.getElementsByTagName('div')[index].innerHTML = str;
             }
         }, 50);
     }, 0);
@@ -20,3 +21,11 @@ setTimeout(function(){
     document.getElementById('poem').style.opacity = "1";
     makePoem();
 },1000);
+
+const startPredict=()=>{
+    const loadDOM=document.getElementById('loading');
+    loadDOM.classList.remove('d-none');
+    setTimeout(function(){
+        loadDOM.classList.add('d-none');
+    },3000)
+}
