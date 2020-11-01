6c0420ea39ca774b18f0a469b169c9fe16394125
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2020-11-01 21:31:45 +0800

    add footer

--- js/index.js
+++ js/index.js
@@ -1,14 +1,22 @@
-var str = document.getElementsByTagName('div')[4].innerHTML.toString();
-var i = 0;
-document.getElementsByTagName('div')[4].innerHTML = "";
 
-setTimeout(function() {
-    var se = setInterval(function() {
-        i++;
-        document.getElementsByTagName('div')[4].innerHTML = str.slice(0, i) + "|";
-        if (i == str.length) {
-            clearInterval(se);
-            document.getElementsByTagName('div')[4].innerHTML = str;
-        }
-    }, 50);
-},0);
+const makePoem = () => {
+    var str = document.getElementsByTagName('div')[4].innerHTML.toString();
+    var i = 0;
+    document.getElementsByTagName('div')[4].innerHTML = "";
+
+    setTimeout(function () {
+        var se = setInterval(function () {
+            i++;
+            document.getElementsByTagName('div')[4].innerHTML = str.slice(0, i) + "|";
+            if (i == str.length) {
+                clearInterval(se);
+                document.getElementsByTagName('div')[4].innerHTML = str;
+            }
+        }, 50);
+    }, 0);
+}
+
+setTimeout(function(){
+    document.getElementById('poem').style.opacity = "1";
+    makePoem();
+},1000);
